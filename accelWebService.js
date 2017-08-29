07bfe22429e3f4bc2a95dc063e0456c8edbb7c30
Nithin Devang <devangnithin@users.noreply.github.com>
2017-08-29 18:43:38 -0500

    Minor fixes

--- accelWebService.js
+++ accelWebService.js
@@ -53,13 +53,13 @@ exports.accelPush = function (req, res) {
             var body = null;
             try {
                 body = JSON.parse(reqBody);
-                console.log(reqBody);
+                //console.log(reqBody);
                 if (count < 1000) {
                     requestArray.push(body);
                     count = count + 1;
                     console.log("data_queued' :'" + reqUrl.pathname );
                     res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end("{'data_queue' :'" + reqUrl.pathname + "'}" + count);
+                    res.end("{'data_queue' :'" + reqUrl.pathname + "', count: "+count+"}");
                 } else {
                     insertTimeSeries(buildDoc(requestArray), res);
                     count = 0;
